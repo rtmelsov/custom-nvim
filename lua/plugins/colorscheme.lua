2e80dc536426a6a49fa21dbc15f030cfe5498099
RahatMelsov <rahatmelsovforjob@gmai.com>
2026-04-26 08:53:24 +0500

    feat: добавлена новая тема

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,36 +1,8 @@
 return {
-  -- 1. GitHub Theme
-  {
-    "projekt0n/github-nvim-theme",
-    name = "github-theme",
-    lazy = false,
-    priority = 1000,
-  },
-
-  -- 2. Nightfox (contains Carbonfox)
-  {
-    "EdenEast/nightfox.nvim",
-    lazy = false,
-    priority = 1000,
-  },
-
-  -- 3. Zenbones (requires lush.nvim)
-  {
-    "mcchrish/zenbones.nvim",
-    dependencies = "rktjmp/lush.nvim",
-    lazy = false,
-    priority = 1000,
-  },
-
-  -- Set your default here
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      -- Change this to test the others:
-      -- "github_dark_dimmed"
-      -- "carbonfox"
-      -- "zenbones"
-      colorscheme = "github_dark_dimmed",
-    },
-  },
+  "Mofiqul/vscode.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.cmd.colorscheme("vscode")
+  end,
 }
